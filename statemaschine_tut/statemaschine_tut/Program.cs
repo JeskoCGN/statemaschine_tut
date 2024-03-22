c4fa99300b0acabf63283e4cae35c0cd7cc4ca76
jesko <dj_jesko@icloud.com>
2024-03-22 13:31:01 +0100

    visual studio setup

---
+++ statemaschine_tut/statemaschine_tut/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace statemaschine_tut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
